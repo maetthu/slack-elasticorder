1903f827624b363bc3a7e76783ab62ef7e7f186a
Matthias Blaser <git@mooch.ch>
2016-05-20 22:58:53 +0200

    and cleanup again

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function checkPresence(){
                             }
                         },
                         function(error){
-                            console.log(error);
+                            console.error(error);
                         }
                     );
                 } else if (!presenceMonitor) {
